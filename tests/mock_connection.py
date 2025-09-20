2ad4546c1422dbfcecb84f28553ed9d95ed88b71
Christopher Li <.@.>
2025-09-20 19:12:27 -0400

    bug: fix pytest accidentally testing mock connections

--- tests/mock_connection.py
+++ tests/mock_connection.py
@@ -2,11 +2,15 @@ from scadable.connection import ConnectionFactory, Connection
 
 
 class TestConnectionFactory(ConnectionFactory):
+    __test__ = False
+
     def create_connection(self, api_key, device_id):
         return TestConnection()
 
 
 class TestConnection(Connection):
+    __test__ = False
+
     def __init__(self):
         super().__init__()
         self.handler = None
